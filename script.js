3b0352783772b1c44ae0ccb969561fb4d3bd8643
flo-314 <julianflo1916@gmail.com>
2021-10-06 13:13:17 -0300

    draw añadido

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const factoryPlayers = (player, mark, turn) => {
   return { player, mark, turn };
 };
-const player1 = factoryPlayers("player1", "X", true);
-const player2 = factoryPlayers("player2", "O", false);
-
-const Game = () => {};
 
 const gameBoard = (() => {
   const board = ["", "", "", "", "", "", "", "", ""];
   const domBoard = [];
-
+  const button = document.querySelector("#restartBtn")
+  button.addEventListener("click", () =>{
+    board.fill("");
+    printDom();
+  })
   const getDomBoard = () => {
     let allBoard = document.querySelectorAll(".board");
     allBoard.forEach((element, index) => {
       element.addEventListener("click", () => {
         let player;
-        if (player1.turn == true) {
-          player = player1;
+        if (gameMethods.player1.turn == true) {
+          player = gameMethods.player1;
         } else {
-          player = player2;
+          player = gameMethods.player2;
         }
 
-        putMarker(index + 1, player);
+        gameMethods.putMarker(index + 1, player);
       });
       domBoard.push(element);
     });
@@ -32,12 +32,25 @@ const gameBoard = (() => {
       domBoard[index].textContent = element;
     });
   };
+  return { getDomBoard, printDom, board };
+})();
 
+
+
+
+
+ 
+
+
+const gameMethods = (() => {
+  const player1 = factoryPlayers("player1", "X", true);
+  const player2 = factoryPlayers("player2", "O", false);
+  let drawStatus = true
   const putMarker = (index, player) => {
     let marker = player.mark;
-    if (board[index - 1] == "") {
-      board[index - 1] = marker;
-      printDom();
+    if (gameBoard.board[index - 1] == "") {
+      gameBoard.board[index - 1] = marker;
+      gameBoard.printDom();
 
       if (player == player1) {
         player2.turn = true;
@@ -50,7 +63,7 @@ const gameBoard = (() => {
     checkForWin();
   };
 
-  function checkForWin() {
+  const checkForWin = () => {
     const winningCombinations = [
       [1, 2, 3],
       [4, 5, 6],
@@ -59,24 +72,41 @@ const gameBoard = (() => {
       [2, 5, 8],
       [3, 6, 9],
       [1, 5, 9],
-      [3, 5, 7], 
+      [3, 5, 7],
     ];
     winningCombinations.forEach((element) => {
-if(board[element[0] -1] == "X" && board[element[1] -1] =="X" && board[element[2] -1] == "X"){
-  board.fill("")
-  printDom()
-}
-else if((board[element[0] -1] == "O" && board[element[1] -1] =="O" && board[element[2] -1] == "O")){
-board.fill("")
-printDom()
-}
+      drawStatus = true;
+      checkDraw()
+      if (
+        gameBoard.board[element[0] - 1] == "X" &&
+        gameBoard.board[element[1] - 1] == "X" &&
+        gameBoard.board[element[2] - 1] == "X"
+      ) {
+        gameBoard.board.fill("");
+        gameBoard.printDom();
+      } else if (
+        gameBoard.board[element[0] - 1] == "O" &&
+        gameBoard.board[element[1] - 1] == "O" &&
+        gameBoard.board[element[2] - 1] == "O"
+      ) {
+        gameBoard.board.fill("");
+        gameBoard.printDom();
+      } else if (drawStatus === true) {
+        gameBoard.board.fill("");
+        gameBoard.printDom();
+      }
+    });
+  };
+  const checkDraw = () => {
+    for (let i = 0; i < 9; i++) {
+      const element = gameBoard.board[i];
+        if(element == ""){
+          drawStatus = false
+        }
+    }
+  }; 
 
- })
-  }
-  return { getDomBoard, printDom, putMarker, checkForWin,board };
+
+  return { putMarker, checkForWin, checkDraw, player1, player2,drawStatus };
 })();
 gameBoard.getDomBoard();
-
-//Player Factory
-//GameBoard
-//Play Functions
